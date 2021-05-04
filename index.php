8dba82834c9aa0cdca359bd5be939a0e5f322be1
VasilPetrich <75956056+VasilPetrich@users.noreply.github.com>
2021-05-04 14:31:58 +0300

    1
    1

---
+++ index.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="facebook-domain-verification" content="34sc3dzj620vvj1z2tzzjwp7b0k52t" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <title>Главная страница | Максим Галкин гив</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon"/>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"/>
+    <!-- Google Fonts Roboto -->
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+    />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    <!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2896826647195699');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=2896826647195699&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+</head>
+<body>
+<!-- Start your project here-->
+<div class="container">
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="text-center">
+            <img class="mb-4 avatarf shadow-1-strong"
+                 src="img/ukraine.png"
+                 style="width: 140px; height: 140px;"/>
+            <h5 class="mb-3">Учавствуйте в конкурсе и получайте ценные призы, а так же от 15000р гарантированно!
+            </h5>
+            <p class="mb-3">Жмите «принять участие»</p>
+            <a
+                    class="btn btn-danger btn-lg"
+                    href=""
+                    target="_blank"
+                    role="button"
+                    style="font-size: 1rem"
+            >ПРИНЯТЬ УЧАСТИЕ</a>
+            <br>
+            <br>
+            <span class="badge rounded-pill bg-dark">Официальный конкурс при поддержке крупных брендов</span>
+        </div>
+    </div>
+</div>
+
+<!-- MDB -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<!-- Custom scripts -->
+<script type="text/javascript"></script>
+</body>
+</html>
